12e52de64220112f511217b3104154d20ee4ceee
luucasor <lucas@jornadatecnologia.com>
2023-09-08 16:15:31 -0300

    Instalando middleware redux-multi

--- src/index.jsx
+++ src/index.jsx
@@ -5,11 +5,13 @@ import App from './main/app'
 import { applyMiddleware, createStore } from 'redux'
 import promise from 'redux-promise'
 import { Provider } from 'react-redux'
+import multi from 'redux-multi'
+
 import reducers from './main/reducers'
 
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__
 && window.__REDUX_DEVTOOLS_EXTENSION__()
-const store = applyMiddleware(promise)(createStore)(reducers, devTools)
+const store = applyMiddleware(multi, promise)(createStore)(reducers, devTools)
 ReactDOM.render(
     <Provider store={store}>
         <App />
